cd83aa874dc2168cda1fc18faf687bea2712d2a9
thanos04 <32007904+thanos04@users.noreply.github.com>
2017-12-06 20:34:21 -0500

    Add files via upload

---
+++ dream_team/config.py
@@ -0,0 +1,36 @@
+# config.py
+
+class Config(object):
+    """
+    Common configurations
+    """
+
+    DEBUG = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class DevelopmentConfig(Config):
+    """
+    Development configurations
+    """
+
+    SQLALCHEMY_ECHO = True
+
+class ProductionConfig(Config):
+    """
+    Production configurations
+    """
+
+    DEBUG = False
+
+class TestingConfig(Config):
+    """
+    Testing configurations
+    """
+
+    TESTING = True
+
+app_config = {
+    'development': DevelopmentConfig,
+    'production': ProductionConfig,
+    'testing': TestingConfig
+}
